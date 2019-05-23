03ab33d3a7f0ed51f0fa3152b47ff5fe085b677d
Falachudin Akbar <falachudin17@gmail.com>
2019-05-23 10:24:36 +0700

    Add files via upload
    first commit.

---
+++ Cek_bpm_sensor/Cek_bpm_sensor.ino
@@ -0,0 +1,24 @@
+#include "ESP8266WiFi.h"
+#include <SoftwareSerial.h>
+
+// Language -> Indonesia
+// Inialisai Variabel yang dibutuhkan untuk membaca sensor....................................... 
+int Sinyal;
+int satudenyut = 550;
+int BPM=0;
+unsigned long millisSebelum=0;
+unsigned long saatIniMillis=0;
+int waktumembaca=15000; // 15 second count bpm
+
+//  Variables pin Pulse sensor
+int PulseSensorPurplePin = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  membaca_sensor();
+  delay(1000);
+}
---
+++ Cek_bpm_sensor/method_membaca_sensor.ino
@@ -0,0 +1,21 @@
+ void membaca_sensor(){
+  // pointer untuk membaca sensor selama 15 detik.
+  saatIniMillis = millis();
+  Serial.println("Detik ke : ",saatIniMillis/1000);
+  // pembacaan sensor diletakkan ke variabel Sinyal.
+  Sinyal = analogRead(PulseSensorPurplePin);
+  // Jika sesuai dengan nilai satu denyut akan bertambah 1 variabel BPM.
+  if(Sinyal > satudenyut){
+    BPM+=1;          
+  }
+  // jika sudah melewati 15 detik akan mengirimkan sms jika BPM<60 atau BPM>100 
+  // dan mengirimkan ke Thingspeak.
+  if(saatIniMillis-millisSebelum>=waktumembaca){
+    BPM=BPM*4; // waktumembaca 4 times
+    Serial.print("Hasil Detak jantung=");
+    Serial.println(BPM);
+    Serial.println("---------------------------");
+    BPM=0;
+    millisSebelum=saatIniMillis;
+   }
+ }
